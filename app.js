a0d0072515c984de5e22adbea3a0fe5cea3e251d
worstcodah <rafael4296@gmail.com>
2021-09-26 19:04:06 +0300

    Added jwt strategy.

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const session = require("express-session");
-var passport = require("passport");
-var crypto = require("crypto");
-var routes = require("./routes");
-const connection = require("./config/database");
-
-// Package documentation - https://www.npmjs.com/package/connect-mongo
-const MongoStore = require("connect-mongo");
-
-// Need to require the entire Passport config module so app.js knows about it
-require("./config/passport");
+const cors = require("cors");
+const path = require("path");
+const passport = require("passport");
 
 /**
  * -------------- GENERAL SETUP ----------------
@@ -22,57 +13,36 @@ require("dotenv").config();
 // Create the Express application
 var app = express();
 
+// Configures the database and opens a global connection that can be used in any module with `mongoose.connection`
+require("./config/database");
+
+// Must first load the models
+require("./models/user");
+
+// Pass the global passport object into the configuration function
+require("./config/passport")(passport);
+
+// This will initialize the passport object on every request
+app.use(passport.initialize());
+
+// Instead of using body-parser middleware, use the new Express implementation of the same thing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/**
- * -------------- SESSION SETUP ----------------
- */
+// Allows our Angular application to make HTTP requests to Express application
+app.use(cors());
 
-// TODO
-const dbOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
+// Where Angular builds to - In the ./angular/angular.json file, you will find this configuration
+// at the property: projects.angular.architect.build.options.outputPath
+// When you run `ng build`, the output will go to the ./public directory
+app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.createConnection(process.env.MONGO_URI, dbOptions);
-
-const sessionStore = MongoStore.create({
-  mongoUrl: process.env.MONGO_URI,
-  collection: "sessions",
-});
-
-app.use(
-  session({
-    secret: "some secret",
-    resave: false,
-    saveUninitialized: true,
-    store: sessionStore,
-    cookie: {
-      maxAge: 60 * 60 * 24, // = A day
-    },
-  })
-);
-
-/**
- * -------------- PASSPORT AUTHENTICATION ----------------
- */
-require("./config/passport");
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use((req, res, next) => {
-  console.log(req.session);
-  console.log(req.user);
-  next();
-});
 /**
  * -------------- ROUTES ----------------
  */
 
 // Imports all of the routes from ./routes/index.js
-app.use(routes);
+app.use(require("./routes"));
 
 /**
  * -------------- SERVER ----------------
---
+++ config/passport.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+const User = require("mongoose").model("User");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const pathToKey = path.join(__dirname, "..", "id_rsa_pub.pem");
+const PUB_KEY = fs.readFileSync(pathToKey, "utf8");
+
+// TODO
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: PUB_KEY,
+  algorithms: ["RS256"],
+};
+
+const strategy = new JwtStrategy(options, (payload, done) => {
+  User.findOne({ _id: payload.sub })
+    .then((user) => {
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    })
+    .catch((error) => {
+      done(error, null);
+    });
+});
+
+// TODO
+module.exports = (passport) => {
+  passport.use(strategy);
+};
